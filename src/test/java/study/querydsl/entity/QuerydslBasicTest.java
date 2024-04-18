38aa6f9a8cd1a1c88a5ace0d2f93aa249e789fba
이아름 <areum6321@gmail.com>
2024-04-18 18:05:06 +0900

    기본 문법 - 전체 조회 수 조회 테스트 코드

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -164,4 +164,20 @@ public class QuerydslBasicTest {
 
         assertThat(result.size()).isEqualTo(2);
     }
+
+    //전체 조회 수 조회
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 }
