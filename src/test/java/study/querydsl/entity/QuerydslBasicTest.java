1637973eb929d096565cac537b41316fd9479f36
이아름 <areum6321@gmail.com>
2024-04-18 19:48:33 +0900

    기본 문법 - Join, on절 사용 테스트 코드

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -259,4 +259,23 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /*
+    회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+        JPQL: SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'teamA'
+        SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID=t.id and t.name='teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
