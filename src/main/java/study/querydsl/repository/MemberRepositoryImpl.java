ee07bc5e0f31751f1befc863981b0ce80dcf2377
이아름 <areum6321@gmail.com>
2024-04-25 18:42:12 +0900

    Spring Data 페이징 활용 - Querydsl 페이징 연동, 전체 카운트 한번에 조회(단순한 페이징)

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -41,6 +45,35 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 )
                 .fetch();
     }
+
+    //단순한 페이징 조회
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset()) //몇번째부터
+                .limit(pageable.getPageSize()) //1번 조회할 때 몇번째까지
+                .fetchResults();
+
+        List<MemberTeamDto> content = results.getResults(); //실제 데이터
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total); //데이터 반환
+    }
+
 
     private BooleanExpression usernameEq(String username) {
         return hasText(username) ? member.username.eq(username) : null;
